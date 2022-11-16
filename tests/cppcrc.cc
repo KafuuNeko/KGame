0a6d9d46983715999687a80b5def4dd3b33e235f
Kafuu Neko <kafuuneko@gmail.com>
2022-11-16 11:45:25 +0800

    remove test\core.cc

--- tests/cppcrc.cc
+++ tests/cppcrc.cc
@@ -2,14 +2,7 @@
 #include <string_view>
 #include <vector>
 
-#include "cppcrc/code_crc16.hh"
-#include "cppcrc/code_crc32.hh"
-#include "cppcrc/code_crc64.hh"
-#include "cppcrc/code_crc8.hh"
-#include "cppcrc/code_crcdnp.hh"
-#include "cppcrc/code_crckrmit.hh"
-#include "cppcrc/code_crcsick.hh"
-#include "cppcrc/encoder.hh"
+#include "cppcrc.hh"
 
 int main() {
   std::vector<std::pair<const char *, crc::CodeBase::ShareConstPtr>> codes = {
